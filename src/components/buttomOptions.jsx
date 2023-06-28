837e0265ce5d8e53106630c900801e8f8279e272
sebas-camp <sebasadolfo123@gmail.com>
2023-06-27 20:00:01 -0500

    Update buttomOptions.jsx

--- src/components/buttomOptions.jsx
+++ src/components/buttomOptions.jsx
@@ -7,7 +7,7 @@ export default function Example() {
   const [idioma, setIdioma] = useState('es');
 
   return (
-    <div className="text-right md:block hidden">
+    <div className="text-right md:block hidden invisible">
       <Menu as="div" className="relative inline-block text-left">
         <div>
           <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
